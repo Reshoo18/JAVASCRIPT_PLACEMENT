ed47494640a62a87dbd7446ea11f092a6c3e4f1e
Reshoo-18 <reshooranjan7@gmail.com>
2026-03-21 14:52:29 +0530

    async await

---
+++ ALL_TOPICS/CPAW/asyncAwait.js
@@ -0,0 +1,33 @@
+// function api(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(() => {
+//             console.log("weather data");
+//             resolve(200);
+//         }, 2000);
+//     })
+// }
+
+// async function getWeatherData(){
+//       await api();
+// }
+
+
+
+
+//ASYNC AWAIT 
+function getData(dataId){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        console.log("data",dataId);
+        resolve("success")
+        
+    },3000)
+    })
+}
+async function getAllData(){
+    await getData(1);
+     await getData(2);
+      await getData(3);
+       await getData(4);
+
+}
