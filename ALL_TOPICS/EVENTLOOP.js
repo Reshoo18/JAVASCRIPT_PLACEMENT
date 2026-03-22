012b174febea23e6f248f604883e790167fb3130
Reshoo-18 <reshooranjan7@gmail.com>
2026-03-22 14:23:58 +0530

    event loop

---
+++ ALL_TOPICS/EVENTLOOP.js
@@ -0,0 +1,13 @@
+//CALLSTACK
+//WEB BROWSER API
+//CALLSTACK QUEUE
+
+
+console.log("start")
+console.log("line 2 of code");
+setTimeout(() => {
+    console.log("settime out code");
+
+}, 5000);
+
+console.log("end");
