fc57548367a0989f7b0b343eca1655a1e6fe5cb6
Reshoo-18 <reshooranjan7@gmail.com>
2026-09-21 23:13:31 +0530

    some loop q

--- ALL_TOPICS/PRACTICES/1.js
+++ ALL_TOPICS/PRACTICES/1.js
@@ -1,123 +1,144 @@
-let price = "499"
-let qt ="2"
+// let price = "499"
+// let qt ="2"
 
 
-let Nprice=Number(price)
+// let Nprice=Number(price)
 
-let Nqt=Number(qt)
-
-
-let Total=Nprice * Nqt
-
-console.log(Total)
-console.log(typeof(Total))
+// let Nqt=Number(qt)
 
 
-  let age=" 23 years"
-  let price="499.99"
+// let Total=Nprice * Nqt
 
-  let onlyAge=parseInt(age)
-  let onlyPrice=parseFloat(price)
+// console.log(Total)
+// console.log(typeof(Total))
+
+
+//   let age=" 23 years"
+//   let price="499.99"
+
+//   let onlyAge=parseInt(age)
+//   let onlyPrice=parseFloat(price)
   
-  console.log(onlyAge)
-  console.log(typeof(onlyAge))
-  console.log(onlyPrice)
-  console.log(typeof(onlyPrice))
+//   console.log(onlyAge)
+//   console.log(typeof(onlyAge))
+//   console.log(onlyPrice)
+//   console.log(typeof(onlyPrice))
 
 
-let x = "0";
-let y = 0;
-let z = "hello";
-let w = "";
+// let x = "0";
+// let y = 0;
+// let z = "hello";
+// let w = "";
 
 
-let BoolX=Boolean(x)
-let Booly=Boolean(y)
-let Boolz=Boolean(z)
-let Boolw=Boolean(w)
+// let BoolX=Boolean(x)
+// let Booly=Boolean(y)
+// let Boolz=Boolean(z)
+// let Boolw=Boolean(w)
 
-console.log(BoolX)
+// console.log(BoolX)
 
-console.log(Booly)
+// console.log(Booly)
 
-console.log(Boolz)
+// console.log(Boolz)
 
-console.log(Boolw)
-
-
-let marks = "85";
-let bonus = 5;
+// console.log(Boolw)
 
 
-let Nmarks=Number(marks)
-let TotalMarks=Nmarks + bonus
-
-console.log(TotalMarks)
-console.log(typeof(TotalMarks))
-
-let userInput = "100";
-let actualValue = 100;
-
-  console.log(userInput == actualValue)
-  console.log(userInput === actualValue)
-
-let a = 20;
-let b = 6;
-
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a%b)
-
-let marks = 78;
-
-if(marks > 90){
-    console.log("A")
-}else if(marks >70 ){
-    console.log("B")
-}else if(marks >60){
-    console.log("C")
-}else if(marks > 50){
-    console.log("D")
-}else if(marks >= 40){
-    console.log("E")
-}else{
-    console.log("fail")
-}
+// let marks = "85";
+// let bonus = 5;
 
 
+// let Nmarks=Number(marks)
+// let TotalMarks=Nmarks + bonus
 
-let age = 23;
+// console.log(TotalMarks)
+// console.log(typeof(TotalMarks))
 
+// let userInput = "100";
+// let actualValue = 100;
 
-if(age > 18 && age < 60){
-    console.log("you  are eligible")
-}
+//   console.log(userInput == actualValue)
+//   console.log(userInput === actualValue)
+
+// let a = 20;
+// let b = 6;
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+// let marks = 78;
+
+// if(marks > 90){
+//     console.log("A")
+// }else if(marks >70 ){
+//     console.log("B")
+// }else if(marks >60){
+//     console.log("C")
+// }else if(marks > 50){
+//     console.log("D")
+// }else if(marks >= 40){
+//     console.log("E")
+// }else{
+//     console.log("fail")
+// }
 
 
 
-let age = 65;
-let premium = true;
-
-if(age >=18 && age<60 || premium ){
-    console.log("premium avail")
-}
+// let age = 23;
 
 
-let isLoggedIn = false;
-
-if(!isLoggedIn){
-    console.log("please login")
-}
+// if(age > 18 && age < 60){
+//     console.log("you  are eligible")
+// }
 
 
 
+// let age = 65;
+// let premium = true;
+
+// if(age >=18 && age<60 || premium ){
+//     console.log("premium avail")
+// }
+
+
+// let isLoggedIn = false;
+
+// if(!isLoggedIn){
+//     console.log("please login")
+// }
+
+
+
+// let isLoggedIn = true;
+// let age = 20;
+// let isAdmin = false;
+
+
+// if(isLoggedIn && (age >=18 )|| isAdmin){
+//     console.log("Access guranted")
+// }
+
+// let marks = 75;
+
+// let result = marks >=40 ? "pass":"fail"
+
+// console.log(result)
+
+
+
+let age = 25;
 let isLoggedIn = true;
-let age = 20;
 let isAdmin = false;
+let hasPremium = true;
 
 
-if(isLoggedIn && (age >=18 )|| isAdmin){
+if((isLoggedIn && age>=18 )||isAdmin ||(hasPremium && age>=18)){
     console.log("Access guranted")
+}else{
+   console.log("Access denied")
 }
+
---
+++ ALL_TOPICS/PRACTICES/LoopsAndString.js
@@ -0,0 +1,63 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }\
+
+
+
+// for(let i=1;i<=20;i++ ){
+//     if(i%2!=0){
+//         console.log(i)
+//     }
+// }
+
+
+// let n = 50;
+// let sum =0
+// for (let i=1;i<=50;i++){
+//     if(i%3==0){
+       
+//        sum+=i
+  
+//        console.log(sum)
+//     }
+// }
+
+
+
+// let n = 100;
+
+
+// for(let i =1;i<n;i++){
+//    if((i%3==0||i%5==0)&&(i%15!=0)){
+//   console.log(i)
+//    }
+   
+// }
+
+
+
+// for (let i = 1; i <= 10; i++) {
+
+//     for (let j = 1; j <= 10; j++) { 
+        
+//         console.log(i + " x " + j + " = " + (i * j));
+
+       
+//     }
+
+// }
+
+
+
+let count=0
+for(let i=2;i<100;i++){
+   
+  
+    if(((i==2||i==3||i==5||i==7)||(i%2!=0)&&(i%3!=0)&&(i%4!=0)&&(i%5!=0)&&(i%6!=0)&&(i%7!=0)&&(i%8!=0)&&(i%9!=0))){
+      console.log(i)
+      count++
+
+    }
+    
+}
+console.log("total count",count)
