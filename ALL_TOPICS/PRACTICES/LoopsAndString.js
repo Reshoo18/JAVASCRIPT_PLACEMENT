ae602b32d2398ece1dbedd462f62b456028ebfed
Reshoo-18 <reshooranjan7@gmail.com>
2026-09-22 22:56:04 +0530

    some js question fibonnaci series and reverse a number

--- ALL_TOPICS/PRACTICES/LoopsAndString.js
+++ ALL_TOPICS/PRACTICES/LoopsAndString.js
@@ -49,15 +49,42 @@
 
 
 
-let count=0
-for(let i=2;i<100;i++){
+// let count=0
+// for(let i=2;i<100;i++){
    
   
-    if(((i==2||i==3||i==5||i==7)||(i%2!=0)&&(i%3!=0)&&(i%4!=0)&&(i%5!=0)&&(i%6!=0)&&(i%7!=0)&&(i%8!=0)&&(i%9!=0))){
-      console.log(i)
-      count++
+//     if(((i==2||i==3||i==5||i==7)||(i%2!=0)&&(i%3!=0)&&(i%4!=0)&&(i%5!=0)&&(i%6!=0)&&(i%7!=0)&&(i%8!=0)&&(i%9!=0))){
+//       console.log(i)
+//       count++
 
-    }
+//     }
     
+// }
+// console.log("total count",count)
+
+
+
+// // fibonnaci series
+// let sum=0
+// let a=0
+// let b=1
+// for(let i=1;i<=15;i++){
+//   console.log(a)
+// sum += a
+//   let next=a+b
+//   a=b
+//   b=next
+
+ 
+ 
+
+// }
+//  console.log("Total sum",sum)
+  let num=123456
+for(let i=0;i<6;i++){
+  
+    let Ldig= num % 10
+  
+   num= Math.floor(num/10)
+   console.log(Ldig)
 }
-console.log("total count",count)
